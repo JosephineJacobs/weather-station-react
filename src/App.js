8aa4f3fb04847c7fed291cbdfb9b0aa75cd24087
JosephineJacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-10 19:51:53 +0200

    it didn't load

--- src/App.js
+++ src/App.js
@@ -4,34 +4,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Josephine Jacobs
-        </a>{" "}
-        and is open-source on{" "}
-        <a
-          href="https://github.com/JosephineJacobs/weather-station-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        , published on{" "}
-        <a
-          href="https://weatherstation-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a> {" "}
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Josephine Jacobs
+          </a>{" "}
+          and is open-source on{" "}
+          <a
+            href="https://github.com/JosephineJacobs/weather-station-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          , published on{" "}
+          <a
+            href="https://weatherstation-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>{" "}
+          .
+        </footer>
+      </div>
     </div>
   );
 }
