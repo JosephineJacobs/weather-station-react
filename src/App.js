7c81275b9bdbb5bd4753cd9452fcaded043615fe
JosephineJacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-10 13:38:36 +0200

    added GitHub

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App 
-  </div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by <a href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs" target="_blank" rel="noreferrer">
+        Josephine Jacobs
+        </a>{" "}
+        and is {" "}
+        <a
+          href="https://github.com/JosephineJacobs/weather-station-react" target="_blank" rel="noreferrer"
+        >
+          open-source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default App;
+
