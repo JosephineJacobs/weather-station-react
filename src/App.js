d3881f526cbebe03cac7fe1a7fd6854be5664b22
Josephine Jacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-10-12 14:12:13 +0200

    set up weather frame

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
+    
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        
+       
+        <Weather />
         <footer>
           This project was created by{" "}
           <a
             href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
             target="_blank"
             rel="noopener noreferrer"
+
           >
             Josephine Jacobs
           </a>{" "}
